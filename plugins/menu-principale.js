67d5162de164bc00298953a769cbdf39e35a5fbd
Giuse <perr.gius11@gmail.com>
2025-05-24 13:46:03 +0200

    Update menu-principale.js

--- plugins/menu-principale.js
+++ plugins/menu-principale.js
@@ -32,14 +32,14 @@ let handler = async (message, { conn, usedPrefix }) => {
 🤖 *𝐌𝐞𝐧𝐮 𝐏𝐫𝐢𝐧𝐜𝐢𝐩𝐚𝐥𝐞* 👤
 ────────────────────────
 
-➤ ${usedPrefix}𝐩𝐫𝐨𝐩𝐫𝐢𝐞𝐭𝐚𝐫𝐢𝐨 👑
-➤ ${usedPrefix}𝐟𝐮𝐧𝐳𝐢𝐨𝐧𝐢 🔗
-➤ ${usedPrefix}𝐚𝐝𝐦𝐢𝐧 👥
-➤ ${usedPrefix}𝐠𝐫𝐮𝐩𝐩𝐨 🎉
-➤ ${usedPrefix}𝐨𝐰𝐧𝐞𝐫 👑
-➤ ${usedPrefix}𝐜𝐫𝐞𝐝𝐢𝐭𝐢 💬
-➤ ${usedPrefix}𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐨 ⚠️
-➤ ${usedPrefix}𝐢𝐧𝐟𝐨𝐛𝐨𝐭 ℹ️     
+➤ 👑 ${usedPrefix}𝐩𝐫𝐨𝐩𝐫𝐢𝐞𝐭𝐚𝐫𝐢𝐨 
+➤ 🔗 ${usedPrefix}𝐟𝐮𝐧𝐳𝐢𝐨𝐧𝐢 
+➤ 👥 ${usedPrefix}𝐚𝐝𝐦𝐢𝐧 
+➤ 🎉 ${usedPrefix}𝐠𝐫𝐮𝐩𝐩𝐨 
+➤ 👑 ${usedPrefix}𝐨𝐰𝐧𝐞𝐫 
+➤ 💬 ${usedPrefix}𝐜𝐫𝐞𝐝𝐢𝐭𝐢 
+➤ ⚠️ ${usedPrefix}𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐨 
+➤ ℹ️ ${usedPrefix}𝐢𝐧𝐟𝐨𝐛𝐨𝐭      
 
 ────────────────────────
 ${botName}   -  𝐕𝐞𝐫𝐬𝐢𝐨𝐧𝐞: ${vs}
